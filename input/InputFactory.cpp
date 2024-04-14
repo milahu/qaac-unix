cae2d1be559431509cfd3c6aa7bb738b763b163d
Milan Hauth <milahu@gmail.com>
2024-04-14 19:03:07 +0200

    PathFindExtensionW to strrchr, etc

--- input/InputFactory.cpp
+++ input/InputFactory.cpp
@@ -1,4 +1,10 @@
 #include "InputFactory.h"
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <cstring> // strrchr
+#include <fcntl.h> // open
+#include <unistd.h> // lseek
 #include "win32util.h"
 #ifdef QAAC
 #include "ExtAFSource.h"
@@ -17,15 +23,35 @@ std::shared_ptr<ISeekableSource> InputFactory::open(const char *path)
     if (pos != m_sources.end())
         return pos->second;
 
-    const char *ext = PathFindExtensionW(path);
-    std::shared_ptr<FILE> fp(win32::fopen(path, "rb"));
+    const char *ext = strrchr(path, '.');
+    if (ext != nullptr) {
+        // Move pointer to the position after the dot
+        ++ext;
+    } else {
+        // Handle case when no extension is found
+        ext = "";
+    }
+
+    int fd = ::open(path, O_RDONLY);
+    if (fd == -1) {
+        // Handle file open error
+        throw std::runtime_error("Failed to open file");
+    }
+    std::shared_ptr<FILE> fp(fdopen(fd, "rb"));
+    if (!fp) {
+        // Handle file stream creation error
+        ::close(fd);
+        throw std::runtime_error("Failed to create file stream");
+    }
+
     if (m_is_raw) {
         std::shared_ptr<RawSource> src =
             std::make_shared<RawSource>(fp, m_raw_format);
         m_sources[path] = src;
         return src;
     }
-    if (strutil::wslower(ext) == ".avs")
+
+    if (strcasecmp(ext, "avs") == 0)
         return std::make_shared<AvisynthSource>(path);
 
 #define TRY_MAKE_SHARED(type, ...) \
@@ -36,21 +62,26 @@ std::shared_ptr<ISeekableSource> InputFactory::open(const char *path)
             m_sources[path] = src; \
             return src; \
         } catch (...) { \
-            lseek(fileno(fp.get()), 0, SEEK_SET); \
+            lseek(fd, 0, SEEK_SET); \
         } \
     } while (0)
 
-    TRY_MAKE_SHARED(WaveSource, fp, m_ignore_length);
-    if (!win32::is_seekable(fileno(fp.get())))
+    TRY_MAKE_SHARED(WaveSource, fd, m_ignore_length);
+    // Check if the file is seekable
+    if (lseek(fd, 0, SEEK_CUR) == -1) {
+        ::close(fd);
         throw std::runtime_error("Not available input file format");
+    }
 
-    TRY_MAKE_SHARED(MP4Source, fp);
+    TRY_MAKE_SHARED(MP4Source, fd);
 #ifdef QAAC
-    TRY_MAKE_SHARED(ExtAFSource, fp);
+    TRY_MAKE_SHARED(ExtAFSource, fd);
 #endif
-    TRY_MAKE_SHARED(FLACSource, fp);
+    TRY_MAKE_SHARED(FLACSource, fd);
     TRY_MAKE_SHARED(WavpackSource, path);
-    TRY_MAKE_SHARED(TakSource, fp);
-    TRY_MAKE_SHARED(LibSndfileSource, fp);
+    TRY_MAKE_SHARED(TakSource, fd);
+    TRY_MAKE_SHARED(LibSndfileSource, fd);
+
+    ::close(fd);
     throw std::runtime_error("Not available input file format");
 }
