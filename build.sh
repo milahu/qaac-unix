7d6ce19642ff91b643f97b1129809c1bbc95d756
Milan Hauth <milahu@gmail.com>
2024-04-15 19:38:56 +0200

    add CMakeLists.txt

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+set -x
+
+[ -d build ] || mkdir build
+
+cd build
+
+# clang gives better error messages than gcc
+export CC=clang
+export CXX=clang++
+
+# TODO? make it work with system mp4v2
+#cmake ../ -DWITH_MP4V2=/nix/store/x3i6x4a3j1va0ghb7c5ivwaw6ngzhymg-mp4v2-5.0.1
+cmake ../
+
+make
